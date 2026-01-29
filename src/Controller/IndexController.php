60ec2babbe0e6bb0dde36397ad85359486ceb1a4
fredavr <avrillon.frederique@orange.fr>
2026-01-29 19:09:51 +0100

    feat: first controller route and twig

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'Mon controller index',
+            'firstnames' => [
+                'Anthony',
+                'Bruno',
+                'Fred',
+            ]
+        ]);
+    }
+
+    // Exemple de route multiple
+    #[Route('/create', name: 'create')]
+    #[Route('/edit/{firstname}/{lastname}', name: 'edit')]
+    public function edit(?string $firstname, ?string $lastname = null): Response
+    {
+        return new Response('Hello ' . $firstname . ' ' . $lastname . ' !');
+    }
+}
